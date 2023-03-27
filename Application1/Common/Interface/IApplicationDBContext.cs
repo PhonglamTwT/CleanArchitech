d23631ae6d993ef33f744a1b4ab4ff6ce10010e7
LAM PHONG <LAM PHONG@DESKTOP-7HCED0V>
2023-03-27 11:25:26 +0700

    Add project files.

---
+++ Application1/Common/Interface/IApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDBContext
+    {
+        DbSet<AppSetting> AppSettings { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
